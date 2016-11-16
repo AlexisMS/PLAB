de6346953ba49ffc8166743838fadf2251aeec5e
AlexisMS <alexisora@gmail.com>
2016-11-16 20:55:38 -0200

    final PLAB2 fixes

--- Bloco_Controle.vhd
+++ Bloco_Controle.vhd
@@ -1,75 +1,130 @@
 library ieee;
+
 use ieee.std_logic_1164.all;
 
+
+
 entity Bloco_Controle is
+
 	port(
+
 		clock: in std_logic;
+
 		btn: in std_logic;
+
 		recebeu : in std_logic;
+
 		cont2seg: in std_logic;
+
 		reset: in std_logic;
+
 		envia_sinal: out std_logic;
+
 		recebe_sinal: out std_logic;
+
 		resetDoContador: out std_logic;
+
 		estado_recebe: out std_logic;
+
 		estado_envia: out std_logic;
+
 		estado_espera: out std_logic
+
 		);
+
 end entity;
 
+
+
 architecture ctrl of Bloco_Controle is
+
 	type FSMstates is (INICIO, INSTRUCAO, ENVIA, RECEBE);
 	signal actualState, nextState : FSMstates;
+
 begin
 
+
 	--next-state logic
+
 	PROCESS (actualState, btn, cont2seg, recebeu)
+
 	begin
+
 	nextState <= actualState;
+
 	case actualState is
+
 		when INICIO =>
-			estado_envia <= '0';
-			estado_recebe <= '0';
-			estado_espera <= '1';
+
 			if btn = '1' then
+
 				nextState <= INSTRUCAO;
+
 			end if;
+
 		when INSTRUCAO =>
+
 			if btn = '0' and cont2seg = '0' then
+
 				nextState <= ENVIA;
+
 			elsif cont2seg = '1' then
+
 				nextState <= RECEBE;
+
 			end if;
+
 		when ENVIA =>
-			estado_envia <= '1';
-			estado_recebe <= '0';
-			estado_espera <= '0';
+
 			if recebeu = '1' then
+
 				nextState <= INICIO;
+
 			end if;
+
 		when RECEBE =>
-			estado_envia <= '0';
-			estado_recebe <= '1';
-			estado_espera <= '0';
 			if recebeu = '1' then
+
 				nextState <= INICIO;
+
 			end if;
+
 		end case;
+
 	end process;
+
+
 	
 	--memory
+
 	EM: process(clock, reset) is
+
 	begin
+
 		if reset='1' then
+
 			actualState <= INICIO;
+
 		elsif rising_edge(clock) then
+
 			actualState <= nextState;
+
 		end if;
 	end process;
+
+
 	
 	--output logic
+
 	envia_sinal <= '1' when actualState=ENVIA else '0';
+
 	recebe_sinal <= '1' when actualState=RECEBE else '0';
+
 	resetDoContador <= '1' when actualState=INICIO else '0';
+
+	estado_envia <= '1' when actualState=ENVIA else '0';
+	estado_recebe <= '1' when actualState=RECEBE else '0';
+	estado_espera <= '1' when actualState=INICIO else '1' when actualState=INSTRUCAO else '0';	
+
 
 end architecture;
--- Contador.vhd
+++ Contador.vhd
@@ -1,32 +1,60 @@
 library ieee;
+
 use ieee.std_logic_1164.all;
+
 use IEEE.std_logic_unsigned.all;
 
+
+
 entity contador is
-    port(
-			comecaContagem: in std_logic;
-         CLOCK_50: in std_logic;
-			CONTO2SEG: out std_logic := '0'
-        );
-end contador;
+ 
+   port(
+
+	CLOCK_50: in std_logic;
+	comecaContagem: in std_logic;
+
+        CONTO2SEG: out std_logic := '0'
+
+   	);
+
+end entity;
+
+
 
 architecture cont_arch of contador is
-signal contador: std_logic_vector(27 downto 0) := (others => '0');  
- 
+
+	type FSMstates is (PARADO, CONTANDO, CONTOU2SEGUNDOS);
+	signal actualState, nextState : FSMstates;
+	signal contador: std_logic_vector(27 downto 0) := (others => '0');
 begin
-    process(CLOCK_50)
-    begin
-		  if comecaContagem = '0' then
-				contador <= x"0000000";
-		
-        elsif CLOCK_50'event and CLOCK_50 = '1' and comecaContagem = '1' then
-			contador <= contador + 1;
-            if contador > x"5F5E0FF" then
-                CONTO2SEG <= '1';
-            else
-                CONTO2SEG <= '0';            
-            end if;
-			else
-        end if;
-    end process;
-end cont_arch;
+
+	--next-state logic    
+PROCESS (actualState, contador, clock)
+
+    	begin
+
+	nextState <= actualState;
+	case actualState is
+		when PARADO =>
+			if comecaContagem = '1' then
+				nextState <= CONTANDO;
+			end if;
+		when CONTANDO =>
+			if contador > x"5F5E0FF" then
+				nextState <= CONTOU2SEGUNDOS;
+			end if;
+		when CONTOU2SEGUNDOS =>
+			nextState <= PARADO;
+	end case;
+end process;
+	--memory
+EM: PROCESS (CLOCK_50)	is
+	begin
+		if rising_edge(CLOCK_50) then
+			actualState <= nextState;
+		end if;
+end PROCESS;
+	--output logic
+	contador <= x"0000000" when actualState=PARADO else contador+1 when actualState=CONTANDO else contador;
+	CONTO2SEG <= '1' when actualState=CONTOU2SEGUNDOS else '0';
+end architecture;
